fbe2414d642367c00f851d823cc54e8260247f66
Naor Ami <naoric@gmail.com>
2013-09-07 15:48:57 +0300

    (js) Modified the app obj to support inheritance

--- www/js/utils.js
+++ www/js/utils.js
@@ -1,4 +1,37 @@
-var get = function(elem) {
-  return document.getElementById(elem);
-  
-};
+var app = (function(w, d) {
+  if (!Object.hasOwnProperty('create')) {
+    Object.create = function(parentObj) {
+      function tmpObj() {}
+      tmpObj.prototype = parentObj;
+      return new tmpObj();
+    }
+  }
+
+  if (!Object.hasOwnProperty('defineProperties')) {
+    Object.defineProperties = function(obj, props) {
+      for (var prop in props) {
+        Object.defineProperty(obj, prop, props[prop]);
+      }
+    };
+  }
+
+  var ret = {
+    init: function(cb) {
+      if (typeof cb === "function") {
+        document.addEventListener("deviceready", cb, false);
+      }
+    }, 
+    create: function(parent, props) {
+      if (typeof props === "undefined") {
+        return Object.create(parent)
+      } else {
+        return Object.create(parent, props);
+      }
+    }, 
+    get: function(id) {
+      return d.getElementById(id);
+    }
+  }
+
+
+})(window, document);
