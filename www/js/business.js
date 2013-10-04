55c0d060e177b96aff3e3f914a683ed4ddb5b415
alonic <alongoldboim@yahoo.ca>
2013-10-04 05:19:01 -0700

    ajax call to server in order to get business is working

--- www/js/business.js
+++ www/js/business.js
@@ -1,25 +1,21 @@
 (function(w,d){
 $.ajax({
-url:"",
-dataType:"json"
+url: "http://mycard.com/cards/all-cards" ,
+dataType:"jsonp"
+}).done(function( data ) {
 
-})
-.done(function( data ) {
-    var len=data.length
-    var i=0;
+    var len=data.length,
+        i=0;
+        $ulist=$("#ulist"); 
     for(i=0;i<len;i++){
     data[i].id;
    
-   $("#ulist").append("<li><a href='businessPage.html'><img src='http://naoric-PC/image.php/"+data[i].image_id+" /><h3>"+data[i].name+"</h3><p >+" data[i].description+"</p></a><a href='shnizel.html' data-icon='plus' data-iconpos='notext' data-theme='c' data-inline='true'>A</a></li>"
-    }   
     
-    
-    }
-  });
+     $ulist.append("<li><a href='businessPage.html'><img width='50' src='http://naoric-PC/image.php/"+data[i].image_id+"'/><h3>"+data[i].name+"</h3><p >"+data[i].description+"</p></a><a href='shnizel.html' data-icon='plus' data-iconpos='notext' data-theme='c' data-inline='true'>A</a><input type='hidden' value='"+data[i].id+"'/></li>");
+       $ulist.listview('refresh');   
+    }    
+}).fail(function(j, t, e) {
+  alert(e);  
+});
 
-
-
-
-
-
-}(window,document)
+})(window,document);
