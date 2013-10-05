2ee6bee977268f5218c09bc9e2b6b81d92803524
Naor Ami <naoric@gmail.com>
2013-10-05 17:05:38 +0300

    android new build

--- platforms/android/assets/www/js/utils.js
+++ platforms/android/assets/www/js/utils.js
@@ -1,4 +1,39 @@
-var get = function(elem) {
-  return document.getElementById(elem);
+var app = (function (w, d) { "use strict";
   
-};
+  if (!Object.hasOwnProperty('create')) {
+    Object.create = function (parentObj) {
+      function tmpObj() {}
+      tmpObj.prototype = parentObj;
+      return new tmpObj();
+    }
+  }
+
+  if (!Object.hasOwnProperty('defineProperties')) {
+    Object.defineProperties = function(obj, props) {
+      for (var prop in props) {
+        Object.defineProperty(obj, prop, props[prop]);
+      }
+    };
+  }
+
+  var ret = {
+    init: function(cb) {
+      if (typeof cb === "function") {
+        document.addEventListener("deviceready", cb, false);
+      }
+    }, 
+    create: function(parent, props) {
+      if (typeof props === "undefined") {
+        return Object.create(parent)
+      } else {
+        return Object.create(parent, props);
+      }
+    }, 
+    get: function(id) {
+      return d.getElementById(id);
+    }
+  };
+  
+  return ret;
+
+})(window, document);
