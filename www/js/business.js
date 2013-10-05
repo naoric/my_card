7c3c970e54dcdf17b7f5f4321aa31412b5ccec41
alonic <alongoldboim@yahoo.ca>
2013-10-05 06:52:35 -0700

    Extellent additions

--- www/js/business.js
+++ www/js/business.js
@@ -18,4 +18,43 @@ dataType:"jsonp"
   alert(e);  
 });
 
+    
+    
+    
+    
+    
+   $("#ulist").on("click","li>a:nth-child(2)",function(e){
+       
+    var theme = "b",
+        $this = $(this),
+        $row = $this.closest('li');
+       
+    $row.find('.ui-link-inherit')
+                .removeClass('ui-btn-up-a ui-btn-up-b ui-btn-up-c ui-btn-up-d ui-btn-up-e ui-btn-hover-a ui-btn-hover-b ui-btn-hover-c ui-btn-hover-d ui-btn-hover-e')
+                .addClass('ui-btn-up-' + theme)
+                .attr('data-theme', theme);
+       
+//    $.mobile.activePage.find('.ui-btn')
+//                       .removeClass()
+//                       .addClass('ui-btn-up-' + theme)
+//                       .attr('data-theme', theme);
+//    $.mobile.activePage.find('.ui-header, .ui-footer')
+//                       .removeClass('ui-bar-a ui-bar-b ui-bar-c ui-bar-d ui-bar-e')
+//                       .addClass('ui-bar-' + theme)
+//                       .attr('data-theme', theme);
+//    $.mobile.activePage.removeClass('ui-body-a ui-body-b ui-body-c ui-body-d ui-body-e')
+//                       .addClass('ui-body-' + theme)
+//                       .attr('data-theme', theme);
+//           event.preventDefault();
+       
+   
+   e.preventDefault();
+   
+   
+   }); 
+    
+    
+    
 })(window,document);
+
+
