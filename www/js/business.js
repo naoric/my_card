e8f0478b6ec1d27a4d62c2d4931afe27fefe9758
alonic <alongoldboim@yahoo.ca>
2013-10-28 10:28:18 +0200

    business page search with ajax completed

--- www/js/business.js
+++ www/js/business.js
@@ -28,7 +28,7 @@ $( document ).on( "pageinit", "#myPage", function() {
 
 (function(w,d){
  $.ajax({
- url: "http://mycard.com/cards/all-cards" ,
+ url: "Http://naorami.com/cards/all-cards" ,
  dataType:"json",
 	 type:"GET"
  }).done(function( data ) {
@@ -44,7 +44,7 @@ for (i = 0; i < len; i++) {
     $ulist.append("<li><a href='businessPage.html'><img width='50' src='http://mycard.com/" + data[i].logo_path + "'/><h3>" + data[i].name + "</h3><p >" + data[i].description + "</p></a><a href='shnizel.html' data-icon='plus' data-iconpos='notext' data-theme='c' data-inline='true'>A</a><input type='hidden' value='" + data[i].id + "'/></li>");
   
 	
-	$slist.append("<li><a href='#'>"+data[i].name+"</a></li>");
+	$slist.append("<li class='ui-screen-hidden'><a href='#'>"+data[i].name+"</a></li>");
 }
 $slist.listview('refresh');
 	   $ulist.listview('refresh');
