b400a00d8c59bc2a57c683ffe18cd545909ddfad
Naor Ami <naoric@gmail.com>
2013-11-07 09:42:07 +0000

    barcode scanner plugin added

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,17 +1,24 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins\\org.apache.cordova.core.device-motion\\www\\Acceleration.js",
+        "file": "plugins/org.apache.cordova.core.device-motion/www/Acceleration.js",
         "id": "org.apache.cordova.core.device-motion.Acceleration",
         "clobbers": [
             "Acceleration"
         ]
     },
     {
-        "file": "plugins\\org.apache.cordova.core.device-motion\\www\\accelerometer.js",
+        "file": "plugins/org.apache.cordova.core.device-motion/www/accelerometer.js",
         "id": "org.apache.cordova.core.device-motion.accelerometer",
         "clobbers": [
             "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.barcodescanner",
+        "clobbers": [
+            "plugins.barcodeScanner"
         ]
     }
 ]
