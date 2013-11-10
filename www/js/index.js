2c707f550c6cf5607d0b6fd71a4845fb2bb9f321
Naor Ami <naoric@gmail.com>
2013-11-10 08:41:10 +0000

    removed all pages

--- www/js/index.js
+++ www/js/index.js
@@ -6,4 +6,7 @@
         // @todo Check if user is already logged in
         setTimeout(function () { $.mobile.navigate('#login', { transition: 'slideup' }); }, 2000);
     });
+    $doc.on('pageinit', '#login', function (e) {
+        // @todo Login form validation stuff
+    });
 }(jQuery));
