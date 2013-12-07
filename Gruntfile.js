00a8c7345b690f4ee2f5be7f77b6f3a17c69ab10
naoric <naoric@gmail.com>
2013-12-07 16:49:14 +0200

    added grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		less : {
+			dev : {
+				options : {
+					paths : ["www/css"],
+					compress : false
+				},
+				files : {
+					"www/css/redigo.css" : "www/css/redigo.less"
+				}
+			},
+			node : {
+				options : {
+					paths : ['www/css'],
+					compress : true
+				},
+				files : {
+					"www/css/min/*.min.css" : "www/css/*.less"
+				}
+			}
+		},
+		watch : {
+			files : "www/css/*.less",
+			tasks : ["less"]
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-install-dependencies');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['less:node', 'less:dev']);
+};
