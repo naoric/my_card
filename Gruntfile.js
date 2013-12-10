18ce948cb355831503a64b911b72d2bb264f6cd9
Naor Ami <naoric@gmail.com>
2013-12-10 20:21:07 +0200

    css organization changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,21 @@ module.exports = function (grunt) {
     less: {
       dev: {
         options: {
-          paths: ["www/css"],
+          paths: ["www/css/less/"],
           compress: false
         },
         files: {
           "www/css/redigo.css": "www/css/redigo.less"
         }
       },
-      node: {
+      min: {
         options: {
-          paths: ['www/css'],
+          paths: ['www/css/less/'],
           compress: true
         },
-        files: [{
-          expand: true,
-          cwd: 'www/',
-          src: ['css/*.less'],
-          dest: 'www/css/min/',
-          ext: '.min.css'
-        }]
+        files: {
+          "www/css/min/redigo.min.css": "www/css/less/redigo.less"
+        }
       }
     },
     sass: {
